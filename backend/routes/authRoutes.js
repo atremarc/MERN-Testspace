961a3a3ae9a2bd3b7a9f045d06b1667f64ab3e7a
ATREMARC <atremarc@holycross.edu>
2019-11-22 15:30:33 -0500

    added logic to check db for user

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -24,15 +24,28 @@ async function verify(token, client) {
   console.log('User ID: ' + userid);
   console.log('Domian: ' + domain);
   console.log('Email: ' + email);
+
+  var result = '';
+
+  User.find({email: email}, (error, user) => {
+    if(error) {
+      result = error;
+    } else if (user.length === 0) {
+      result = 'this user is not in the database';
+    } else {
+      result = 'this user is in the database';
+    }
+    console.log(result);
+  });
 }
 
 //recieve token id from frontend
 router.post('/google', (req, res) => {
   const body = req.body.tokenID;
-  console.log(body);
   const client = new OAuth2Client(keys.google.clientID);
+
   verify(body, client).catch(console.error);
-  return res.send('token sent');
+  return res.send('token sent to backend');
 });
 
 module.exports = router;
